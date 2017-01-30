34075de031105e34d1be1f3be8dc5e3514101b20
Johannes Untoro <jo@venmond.com>
2017-01-30 16:51:09 +0800

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -9,6 +9,8 @@ describe('Reducers', () => {
                 type: 'SET_SEARCH_TEXT',
                 searchText: 'dog'
             }
+            
+            // Deep freeze is a module to ensure that we use pure function in the reducers if not the test will throw error
             var res = reducers.searchTextReducer(df(''), df(action));
             
             expect(res).toEqual(action.searchText);
@@ -19,9 +21,59 @@ describe('Reducers', () => {
             var action = {
                 type: 'TOGGLE_SHOW_COMPLETED',
             };
+            
+            // Deep freeze is a module to ensure that we use pure function in the reducers if not the test will throw error
             var res = reducers.showCompletedReducer(df(false), df(action));
             
             expect(res).toEqual(true);
+        });
+    });
+    
+    describe('todosReducer', () => {
+        it('should add new todo', () => {
+            var action = {
+                type: 'ADD_TODO',
+                text: 'Going to mall'
+            };
+
+            // Deep freeze is a module to ensure that we use pure function in the reducers if not the test will throw error
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0].text).toEqual(action.text);
+        });
+        
+        it('should toggle todo', () => {
+            var action = {
+                type: 'TOGGLE_TODO',
+                id: 2
+            };
+            
+            var exampleTodos = [
+                {
+                    id: 1,
+                    text: 'Going to mall',
+                    completed: false,
+                    completedAt: undefined
+                }, {
+                    id: 2,
+                    text: 'Working',
+                    completed: true,
+                    completedAt: 1232028534
+                }, {
+                    id: 3,
+                    text: 'Drinking',
+                    completed: false,
+                    completedAt: undefined
+                }
+            ];
+
+            // Deep freeze is a module to ensure that we use pure function in the reducers if not the test will throw error
+            var res = reducers.todosReducer(df(exampleTodos), df(action));
+            //console.log(res);
+            expect(res[1].completed).toEqual(false);
+            expect(res[1].completedAt).toEqual(undefined);
+            //expect(res[0].text).toEqual(action.text);
         });
     });
 });
