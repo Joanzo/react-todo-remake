b434ad4f108a8feb5b0234cec5af92d3a95f5230
Johannes Untoro <jo@venmond.com>
2017-02-04 12:51:00 +0800

    Add actions for firebase login and logout

--- app/app.jsx
+++ app/app.jsx
@@ -3,12 +3,16 @@ var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
 var {Route, Router, IndexRoute, hashHistory} =  require('react-router');
 
-var TodoApp = require('TodoApp');
-//var Login = require('Login');
-
 var actions = require('actions');
 var store = require('configureStore').configure();
 var TodoAPI = require('TodoAPI');
+
+
+import Login from 'Login';
+import TodoApp from 'TodoApp';
+
+
+
 
 /*store.subscribe(() => {
     var state = store.getState();
@@ -30,10 +34,10 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
     <Provider store={store}>
-      <TodoApp />
+
         <Router history={hashHistory}>
-          <Route path="/" component={Login}>
-            <Route path="todo" component={TodoApp}/>
+          <Route path="/">
+            <Route path="todos" component={TodoApp}/>
             <IndexRoute component={Login}/>
           </Route>
         </Router>
--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,13 +1,25 @@
 var React = require('react');
+import * as Redux from 'react-redux';
 
-var TodoApp = React.createClass({
+import * as actions from 'actions';
+
+export var Login = React.createClass({
+    onLogin() {
+        var {dispatch} = this.props;
+        dispatch(actions.startLogin());
+    },
     render: function() {
         return (
             <div>
-                <h1 className="page-title">Login Page</h1>
+                <h1 className="page-title">Todo App</h1>
                 <div className="row">
                     <div className="column small-centered small-11 medium-6 large-5">
-                        <div className="container">
+                        <div className="callout callout-auth">
+                            <h3>Login</h3>
+                            <p>
+                                Login with Github account below.
+                            </p>
+                            <button className="button" onClick={this.onLogin}>Login with Github</button>
                         </div>
                     </div>
                 </div>
@@ -15,5 +27,4 @@ var TodoApp = React.createClass({
         );
     }
 });
-
-module.exports = TodoApp;
+export default Redux.connect()(Login);
